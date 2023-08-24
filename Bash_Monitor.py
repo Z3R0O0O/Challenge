b9f4dba1400971ca9dc40c5f88785012e314bd8d
Z3R0O0O <127508635+Z3R0O0O@users.noreply.github.com>
2023-08-24 15:42:04 -0300

    Bash_Monitor.py

--- Bash_Monitor.py
+++ Bash_Monitor.py
@@ -13,20 +13,12 @@ palavras = ['su', 'sudo']                               # Palavras a serem procu
 
 # Classe para manipular eventos
 class EventHandler(ProcessEvent):                       # Personalizada para lidar com eventos específicos.
-    def __init__(self):
-        self.contando_sudo = 0
-        self.contando_su = 0
-    
     def process_IN_MODIFY(self, evento):                 # É chamada quando ocorrem modificações em arquivos no diretório monitorado.
         if evento.pathname == caminho:
             with open(caminho, 'r') as f:
                 conteudo = f.read()
                 for chaves in palavras:
                     if chaves in conteudo:
-                        if chaves == 'sudo':
-                            self.contando_sudo += 1
-                        elif chaves == 'su':
-                            self.contando_su += 1
                         print(f"A palavra '{chaves}' foi encontrada em {arquivo}")
                         subprocess.run(['bash', '-i'])
 
@@ -40,18 +32,15 @@ notifier = Notifier(wm, handler)        # Manipulador de eventos, recebe e proce
 monitor = wm.add_watch(diretorio, mask, rec=True)      
 print(f"Monitorando {diretorio}/{arquivo}")
 
-while True: 
+if __name__ == "__main__":
     try:
         while True:
             try:
-                notifier.process_events()   # ProcessEnvent-Herda o a configuração do manipulador de eventos personalizado para os eventos do pyinotify.
+                notifier.process_events() # ProcessEnvent-Herda o a configuração do manipulador de eventos personalizado para os eventos do pyinotify.
                 if notifier.check_events():
                     notifier.read_events()
-                print(f"'sudo' foi encontrado {handler.contando_sudo} vezes.") # Exibir as contagens das palavras encontradas 
-                print(f"'su' foi encontrado {handler.contando_su} vezes.")
-            except KeyboardInterrupt:       # Caso encerre o código manualmente
+            except KeyboardInterrupt:
                 notifier.stop()
                 break
     finally:
-        print(f"Total de vezes que 'sudo' foi encontrado: {handler.contando_sudo}") # Mostra a contagem final
-        print(f"Total de vezes que 'su' foi encontrado: {handler.contando_su}")
+        print("Monitoramento encerrado.")
